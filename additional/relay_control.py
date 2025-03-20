3ad2cee889b4931b65dec65cbe770f295dde95ee
Wolfgang Hinzmann <wolfgang.hinzmann@doccheck.com>
2025-03-20 23:35:15 +0100

    WIP

---
+++ additional/relay_control.py
@@ -0,0 +1,25 @@
+import sys
+import RPi.GPIO as GPIO
+
+# GPIO Pin number
+RELAY_PIN = 4
+
+# Use BCM numbering
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+
+if len(sys.argv) != 2:
+    print("Usage: python3 relay_control.py <on|off>")
+    sys.exit(1)
+
+command = sys.argv[1]
+
+if command == "on":
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+    print("Relay is ON - GPIO LOW")
+elif command == "off":
+    GPIO.output(RELAY_PIN, GPIO.HIGH)
+    print("Relay is OFF - GPIO HIGH")
+else:
+    print("Invalid command")
+    sys.exit(1)
