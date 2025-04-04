f2e00e8aa8fbf2ac92eb38ef0a02266ac696e058
Wolfgang Hinzmann <wolfgang.hinzmann@doccheck.com>
2025-04-04 19:33:00 +0200

    WIP

--- additional/rotate_screen.sh
+++ additional/rotate_screen.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+# Ensure environment variables are set
 export XDG_RUNTIME_DIR=/run/user/1000
 export DISPLAY=:0
+
+# Check and set ACL permissions if necessary
+if [ ! -r /run/user/1000/wayland-0 ]; then
+  sudo setfacl -m u:www-data:rwx /run/user/1000
+  sudo setfacl -m u:www-data:rw /run/user/1000/wayland-0
+fi
+
+# Run the Wayland commands
 wlr-randr --output HDMI-A-2 --mode 1920x1080
 wlr-randr --output HDMI-A-2 --transform 90
