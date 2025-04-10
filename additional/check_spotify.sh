ab034ca507ee06fb677142d8dce879973cd37896
Wolfgang Hinzmann <wolfgang.hinzmann@doccheck.com>
2025-04-10 20:09:13 +0200

    WIP

--- additional/check_spotify.sh
+++ additional/check_spotify.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
- /path/to/php /path/to/your/project/bin/console app:check-spotify-is-running
+ /usr/bin/php /home/master/digital-frame/bin/console app:check-spotify-is-running
  sleep 30
- /path/to/php /path/to/your/project/bin/console app:check-spotify-is-running
+ /usr/bin/php /home/master/digital-frame/bin/console app:check-spotify-is-running
